5d5b542185fb46311f4b2005e4eb88325f7e798e
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2021-01-11 18:17:57 +0800

    [GUI][更新趋势策略]

--- Stock/TrendData.py
+++ Stock/TrendData.py
@@ -10,14 +10,16 @@ class TrendData():
         self.pro = ts.pro_api('ac147953b15f6ee963c164fc8ee8ef5228e58b75e5953ba5997ef117')
         # ac147953b15f6ee963c164fc8ee8ef5228e58b75e5953ba5997ef117
         ts.set_token('ac147953b15f6ee963c164fc8ee8ef5228e58b75e5953ba5997ef117')
+        self.stock_basic= self.pro.stock_basic()
+
     def get_trend_data(self):
         result_df = pd.DataFrame()
         today = (datetime.date.today()- relativedelta(days=+1)).strftime('%Y%m%d')
         start_date = (datetime.date.today() - relativedelta(years=+1)).strftime('%Y%m%d')
 
-        basic = self.pro.daily_basic(trade_date=today)
-        print( basic )
-        for index, row in basic.iterrows():
+        daily = self.pro.daily_basic(trade_date=today)
+        print( daily )
+        for index, row in daily.iterrows():
             print( index, row )
             df = ts.pro_bar(ts_code=row.ts_code, start_date=start_date, end_date=today, ma=[5, 20, 30])
             # print( df.columns,df)
@@ -28,17 +30,16 @@ class TrendData():
                 one_df['density']= (one_df.loc[0].ma5 +one_df.loc[0].ma20+one_df.loc[0].ma30)/one_df.close -3
 
             result_df=  result_df.append( one_df)
-
+        print( result_df.shape)
+        result_df=pd.merge( result_df, self.stock_basic, on='ts_code', how='left')
+        result_df = result_df.loc[result_df['density'] == 0]
         return  result_df
-
-
-
 
 
 if __name__ == '__main__':
     today = (datetime.date.today() - relativedelta(days=+1)).strftime('%Y%m%d')
     result=TrendData().get_trend_data()
     print( result.shape, result )
-    result.to_csv(today+"trend_data.csv")
+    result.to_csv(today+"_trend_data.csv")
 
 
