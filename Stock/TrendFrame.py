bc433b35a007a1df1267d40d320376f6afb0ff71
xiangshaoxiong <xiangshaoxiong@wavewisdom.com>
2021-01-11 18:32:59 +0800

    [GUI][更新趋势表格]

--- Stock/TrendFrame.py
+++ Stock/TrendFrame.py
@@ -1,6 +1,11 @@
 
 import  tkinter as tk
 from tkinter import  *
+from tkinter import ttk
+import pandas as pd
+
+from Stock.TrendData import TrendData
+
 
 class TrendFrame(Frame ):
     def __init__(self, root=None):
@@ -10,9 +15,47 @@ class TrendFrame(Frame ):
 
 
     def create_paned_window(self, root):
-        self.lable=Label(self, text='trend')
-        self.lable.pack(side=TOP)
-        self.frame = Frame(self)
-        self.frame.pack(fill=BOTH, expand=1)
+        df =TrendData().get_trend_data()
+        columns = df.columns.tolist()
+        self.tree = ttk.Treeview(self, show="headings", columns=columns, selectmode='browse')
+        self.tree.pack()
+        """
+            定义滚动条控件
+        """
+        VScrollX = ttk.Scrollbar(self.tree, orient='horizontal', command=self.tree.xview)
+        VScrollY = ttk.Scrollbar(self.tree, orient='vertical', command=self.tree.yview)
+
+        # 给treeview添加配置
+        self.tree.configure(xscrollcommand=VScrollX.set)
+        self.tree.configure(yscrollcommand=VScrollY.set)
+        # 设置表格文字居中
+        # column(column, option=None, **kw)给各列设置属性，或返回属性。
+        # 第一个column是列标识符.
+        # 第二个option，如果不设置则返回所有属性的字典，如果设置则返回那个属性的值。kw里的option有5个
+        # id：只读属性，返回列名。
+        # anchor：文字在cell里的对齐方式，标准的tk的anchor属性
+        # minwidth: 值，单位是像素，列的最小宽度
+        # stretch: 布尔值，表示列的宽度是否随整个部件的改动而变化。
+        # width：列宽，单位是像素。
+        # 提示：如果要设置树状结构那列，用column=“#0”
+        for col in columns:
+            self.tree.column(col, anchor="center", width=20)
+            # self.tree.heading(col, text=col)
+            self.tree.column('#0', stretch=0)
+            self.tree.heading(col, text=col, command=lambda _col=col: self.treeview_sort_column(self.tree, _col, False))
+
+        for rowIndex in df.index:
+            # print( df.loc[rowIndex].values )
+            self.tree.insert('', rowIndex, values=tuple(df.loc[rowIndex].values))
+        print('-----------')
+
+        self.tree.pack(expand=True, fill='both')
 
 
+if __name__ == '__main__':
+    root = Tk()
+    root.title('数学曲线窗口')
+    root.geometry('320x320+200+200')
+    trendframe = TrendFrame(root)
+    trendframe.pack(fill=BOTH, expand=1)
+    root.mainloop()
